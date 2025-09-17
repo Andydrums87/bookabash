694916ef465be762d110ea51950f3b5ec7b14615
Andrew Joseph <andrewjoseph@Andrews-MacBook-Pro.local>
2025-09-17 15:02:33 +0100

    update next anayltics

--- src/app/preview/layout.jsx
+++ src/app/preview/layout.jsx
@@ -1,10 +1,12 @@
 // app/preview/layout.js 
 import { ToastProvider } from '@/components/ui/toast'
+import { Analytics } from "@vercel/analytics/next"
 
 export default function PreviewLayout({ children }) {
   return (
     <ToastProvider>
       {children}
+      <Analytics />
     </ToastProvider>
   )
 }
